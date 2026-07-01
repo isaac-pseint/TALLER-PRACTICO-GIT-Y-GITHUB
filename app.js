94f8e00ae871a513bf218d2373d21d3712372b93
isaac <blancoisaac316@gmail.com>
2026-07-01 18:54:40 -0500

    se agregaron dos ciudades al js y se agrego un archivo css para mejorar la estetica de la tabla, tambien se hixo un cambio en la etiqueta class de el table

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 let ciudades=[
     {nombre: "Bogota",numeroHabitantes:20000000,metrosSM:25000,temperatura:"15°",coordenadas:[455,-235],sitios:[{"01":"parque","02":"museo"}]},
     {nombre: "Cali",numeroHabitantes:5000000,metrosSM:5000,temperatura:"30°",coordenadas:[120,-280],sitios:[{"01":"plaza","02":"piscina"}]},
-    {nombre: "Pereira",numeroHabitantes:2000000,metrosSM:10000,temperatura:"20°",coordenadas:[220,-180],sitios:[{"01":"zoologico","02":"parque del cafe"}]}]
+    {nombre: "Pereira",numeroHabitantes:2000000,metrosSM:10000,temperatura:"20°",coordenadas:[220,-180],sitios:[{"01":"zoologico","02":"parque del cafe"}]},
+    {nombre: "Medellin",numeroHabitantes:2500000,metrosSM:1500,temperatura:"22°",coordenadas:[62,-355],sitios:[{"01":"parque explora","02":"comuna 13"}]},
+{nombre: "Cartagena",numeroHabitantes:1000000,metrosSM:2,temperatura:"28°",coordenadas:[180,-405],sitios:[{"01":"ciudad amurallada","02":"castillo san felipe"}]},
+]
 
 
 
--- paso_a_paso.md
+++ paso_a_paso.md
@@ -8,3 +8,8 @@
 ### tercer comando
 - git commit . -m "subida inicial de los archivos"
 
+### cuarto comando 
+- git commit -m "se hizo el codigo del index y en el js se creo el objeto y se recorrio con un foreach"
+### quinto comando 
+- git push
+### sexto comando 
